fceab5ecbc22c4a47eaeb3239243f6096ce087a3
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-08-09 17:24:15 -0400

    Add Archive Page info gathering & Selected instrument processing

--- includes/shortcodes/musicdistro-archive-shortcode.php
+++ includes/shortcodes/musicdistro-archive-shortcode.php
@@ -22,12 +22,6 @@ function musicdistro_archive_shortcode( $atts ) {
 	), $atts, 'musicdistro' );
 
     
-    
-    //-- SET VARS --//
-    
-    // Attributes
-    $band =      $atts['band'];
-    
     // Output
     $output = '';
     
@@ -43,6 +37,42 @@ function musicdistro_archive_shortcode( $atts ) {
     // Default
     $button_classes .= 'button ';
     
+    
+
+    //-- ARCHIVE PAGE INFORMATION --//
+    
+    // Array for Storing Options
+    $options[] = '';
+    
+    
+    // SLUG of the band (parent category)
+    $options['band_slug'] = $atts['band'];
+
+
+    // Get the whole term BY slug, using the BAND SLUG (Parent Category Slug) 
+    // as the term, INSIDE the download_category taxonomy
+    $options['band_term'] = get_term_by( 'slug' , $options['band_slug'], 'download_category' );
+
+
+    // Get the ID from that term
+    $options['band_id'] = $options['band_term']->term_id;
+
+
+    // Get the Name from that term also
+    $options['band_name'] = $options['band_term']->name;
+    
+
+    
+    //-- SELECTED INSTRUMENT INFORMATION --//
+
+    // Get selected instrument
+    $selected = isset($_REQUEST['cat']) && $_REQUEST['cat'] != '' ? $_REQUEST['cat'] : 0;
+
+
+    $selected_instrument_id = $selected;                                                                // ID of selected instrument
+    $selected_instrument_term = get_term_by('id', $selected_instrument_id, 'download_category');        // TERM of selected instrument
+    $selected_instrument_slug = $selected_instrument_term->slug;                                        // SLUG of selected instrument
+    $selected_instrument_name = $selected_instrument_term->name;                                        // NAME of selected instrument
 
     
     
