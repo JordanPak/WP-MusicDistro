7f097ee704994df0aa1c2af67873f396f068d00e
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-08-09 15:11:54 -0400

    Init plugin main file & copy/mod header from old "EDD Music" plugin by Chris Christoff.

---
+++ wp-music-distro.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: WP MusicDistro
+Plugin URI:  http://JordanPak.com/
+Description: Music Distribution System Easy Digital Downloads Addon for Bands
+Version:     2.0
+Author:      Jordan Pakrosnis
+Author URI:  http://JordanPak.com/
+*/
+
+
+//===================//
+//  DASHBOARD SETUP  //
+//===================//
+
+require_once( 'admin.php' );
