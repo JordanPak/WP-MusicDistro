517933a2575d45efcd28204c7f0238d82c887e4b
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-08-09 17:58:05 -0400

    Copy archive logic from old repo; working to a point...

--- includes/shortcodes/musicdistro-archive-shortcode.php
+++ includes/shortcodes/musicdistro-archive-shortcode.php
@@ -95,6 +95,283 @@ function musicdistro_archive_shortcode( $atts ) {
     
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    //-- IF AN INSTRUMENT HAS BEEN SELECTED --//
+    if($selected)
+    {	
+
+        //-- ARRANGEMENTS WP_QUERY OPTIONS --//
+        $arrangementSelection = array(
+            'post_type'			=> 'download',
+            'download_category'	=> $selected_instrument_slug,
+            'fields' => 'ids', // This is so only the ID is returned instead of the WHOLE post object (Performance)
+            'orderby' => 'title',
+            'order' => 'ASC',
+            'posts_per_page' => -1
+        );                                            
+
+
+        //----------------------------------------------------//
+        //-- ARRAY OF ALL SONGS FOR THE SELECTED INSTRUMENT --//
+        //----------------------------------------------------//
+        $arrangements = new WP_Query( $arrangementSelection );
+
+
+
+        // UCFB_ADDON //
+
+        // If no arrangements: Error
+        if(($arrangements->have_posts()) == false)
+        {
+            echo '<div class="col-md-4"><div class="block"';
+            echo '<div class="alert alert-warning"><i class="fa fa-exclamation-triangle"></i> No ' . $band_name . ' ' . $selected_instrument_name . ' arrangements found!</div>';
+            echo '</div></div>';
+        }
+
+
+
+        // Get Posts associated with arrangements	        
+        $arrangements = $arrangements->get_posts();
+
+
+
+        //----------------------------------------//
+        //-- ARRAY OF ALL TYPES OF SONGS (TAGS) --//
+        //----------------------------------------//                                    
+        $tags = wp_get_object_terms( $arrangements, 'download_tag');										
+
+
+
+        // Remove Duplicate Tags
+        $tags = array_unique($tags, SORT_REGULAR);
+
+
+
+        //---------------------------//
+        //-- CATEGORY BOXES (TAGS) --//
+        //---------------------------//
+        foreach( $tags as $tag )
+        { ?>
+
+            <div class="col-lg-<?php echo $panel_width; ?>">
+
+                <div class="panel <?php echo $panel_theme; ?>">
+
+                    <div class="panel-heading"><?php echo '<' . $panel_header . '>' . $tag->name . '</' . $panel_header . '>'; ?></div>
+
+                    <div class="panel-body">
+
+                        <?php
+
+
+                            //-- CYCLE THROUGH ARRANGEMENTS --//
+
+                            // Just the IDs of the arrangements
+                            foreach($arrangements as $arrangement)
+                            {
+
+                                // Get the arrangement object from the ID	
+                                $object = get_post($arrangement);
+
+
+
+                                //-- CHECK IF CURRENT ARRANGEMENT HAS TAG FOR THIS BOX --//
+                                if( has_term( $tag, 'download_tag', $object ) )
+                                {
+
+
+                                    //-- Display Arrangement Title --//
+                                    echo '<b>' . get_the_title($arrangement) . '</b><div class="dl-btns">';
+
+
+                                    //-- Get Files (Names & URLSs) For Current Arrangement --//
+                                    $files = edd_get_download_files($arrangement);
+
+
+                                    // Set counter for unsetting, keeps track of what index we're at for removing
+                                    $counter_a = 0;
+
+
+
+                                    //-- CYCLE THROUGH FILES OF CURRENT ARRANGEMENT --//
+                                    //--     AND REMOVE UNMATCHING INSTRUMENTS      --//
+                                    foreach( $files as $file){
+
+
+                                        //-- Explode File Into Array of Strings --//
+                                        $explosion = explode(" ", $file['name']);
+
+
+
+                                        //---------------------//
+                                        // TWO WORD INSTRUMENT //
+                                        //------------------------------------------------//
+                                        // If the second word is NOT a number and EXISTS  //
+                                        //------------------------------------------------//
+                                        if( (is_numeric($explosion[1]) == FALSE) && ($explosion[1] != NULL) )
+                                        {
+
+                                            //-- Unset Current File If It's Not For Selected Instrument --//
+                                            if ( ($explosion[0] . ' ' . $explosion[1]) !== $selected_instrument_name )																					
+                                                unset($files[$counter_a]);
+                                        }
+
+
+
+                                        //---------------------//
+                                        // ONE WORD INSTRUMENT //
+                                        //-------------------------------------------//
+                                        // If it's NOT a two word instrument (else)  //
+                                        //-------------------------------------------//
+                                        else {
+
+                                            if ($explosion[0] !== $selected_instrument_name)
+                                                unset($files[$counter_a]);
+                                        }
+
+
+                                        // Increment counter
+                                        $counter_a++;
+                                    }
+
+
+                                    // Sorts the array alphabetically
+                                    asort($files);
+
+
+                                    // Counter for pipes (USING BUTTONS NOW)
+                                    $counter = 1;
+
+
+
+                                    //-- CYCLE THROUGH FILTERED FILES PRINT APPROPRIATE --//
+                                    //--        LINKS FOR THE SELECTED INSTRUMENT       --//
+                                    foreach( $files as $file){
+
+                                        //-- Explode File Into Array of Strings --//
+                                        $explosion = explode(" ", $file['name']);																		
+
+
+                                        //-- If the first word OR first two words = the slected instrument --//
+                                        if (  
+                                              ($explosion[0] == $selected_instrument_name) || 
+                                              (($explosion[0] . ' ' . $explosion[1]) == $selected_instrument_name)
+                                           )
+                                        {
+
+
+                                            // Remove the instrument name and space from file name (variable)
+                                            $name = str_replace($selected_instrument_name." ","",$file['name']);
+
+
+                                            // Output link for file
+                                            // To be updated later to force download when Chris finishes rewritting
+                                            // the disgusting thing that is EDD Free Downloads
+
+
+                                            // Exception for Recordings: Different Icon!
+                                            if($selected_instrument_name == "Recordings") {
+
+                                                echo '<a class="btn btn-xs ' . $dl_btn_theme . '" href="'.$file['file'].'" target="_blank"><i class="fa fa-play"></i></a>';																				
+
+                                            }
+
+
+                                            // Not recording
+                                            else {
+
+                                                // If the arrangment only has one part for a given instrument
+                                                // (Detected by the input name not having a number)
+                                                if ( 
+                                                    ( is_numeric($explosion[1]) == FALSE ) &&
+                                                    ( is_numeric($explosion[2]) == FALSE ) 
+                                                   )
+                                                {
+                                                    echo '<a class="btn btn-xs ' . $dl_btn_theme . '" href="'.$file['file'].'" target="_blank"><i class="' . $single_dl_fa . '"></i></a>';
+                                                }
+
+
+                                                // For sheet music with more than one part for a given instrument
+                                                else {
+                                                    echo '<a class="btn btn-xs ' . $dl_btn_theme . '" href="'.$file['file'].'" target="_blank">' . $name . '</a>';
+                                                }
+
+                                            } // Else: Not recording
+
+
+                                            // If it's not the last item, put in a space
+                                            if ( $counter != count($files)){
+                                                echo '&nbsp';
+                                            }
+
+
+                                            //-- Unset / Reset Array --//
+                                            $explosion = array();
+
+
+                                            $counter++;
+
+
+                                        } // IF the name of the file = selected instrument
+
+
+                                    } // foreach: files as file
+
+
+                                    // Finish dl-btns
+                                    echo '</div>';
+
+
+                                    // Add Spacer
+                                    echo '<hr>';
+
+
+                                } // if: has_term download tag
+
+
+                            } // foreach: arrangements as arrangement
+
+                        ?>
+
+                    </div>
+
+                </div><!-- /.panel-warning -->
+            </div><!-- /.col-md-4 -->						
+
+  <?php } // foreach: $tags
+
+
+
+
+    } // If $selected
+
+
+    /* Restore original Post Data */
+    wp_reset_postdata();    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // Return Output String
 	return $output;
     
