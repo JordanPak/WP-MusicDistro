c63c43986b2deb0161b9c60e3581a7e48bcd398e
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-08-09 16:18:50 -0400

    Init archive settings functions

--- includes/settings/musicdistro-archive-settings.php
+++ includes/settings/musicdistro-archive-settings.php
@@ -7,7 +7,35 @@
  */
 
 
-function musicdistro_archive_options_set( $options ) {
+
+/**
+ * WP MusicDistro Archive Settings: Get Defaults
+ * 
+ * @return      array with default options
+ */
+function musicdistro_archive_options_defaults() {
+
+    $options[] = '';
     
+    $options['band']                    =   'marching-knights';
+    $options['show_header']             =   'false';
+    $options['select_instrument_text']  =   'Get Music';
+    $options['category_box_width']      =   'one-third';
+    $options['single_part_dl_icon']     =   'arrow-down';
+    
+    return $options;
+    
+} // musicdistro_archive_options_set()
+
+
+
+
+/**
+ * WP MusicDistro Archive Settings: Set
+ *
+ * @param       array containing options
+ * @return      array with new options
+ */
+function musicdistro_archive_options_set( $options ) {
     
 } // musicdistro_archive_options_set()
